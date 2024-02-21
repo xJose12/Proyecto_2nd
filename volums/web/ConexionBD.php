ba00835b4aad49bf9e6fc01a13735125dbc43cac
xAna0x <anamariaac219@gmail.com>
2024-02-21 16:33:53 +0100

    modified:   volums/web/ConexionBD.php

--- volums/web/ConexionBD.php
+++ volums/web/ConexionBD.php
@@ -58,11 +58,29 @@
                     if ($resultado->rowCount() > 0) {
                         $row = $resultado->fetch(PDO::FETCH_ASSOC);
                         $Desen_id  = $row['id'];
-                        echo "El desarollador $nombre esta repetido <br>";
+                        echo "El desarollador esta repetido <br>";
                     } else {
                         $conn->exec("INSERT INTO desenvolupador(nombre) VALUES ('$nombre')");
                         $Desen_id = $conn->lastInsertId();
-                        echo "El desarollador $nombre se ha insertado <br>";
+                        echo "El desarollador se ha insertado <br>";
+                    }
+
+                    //Inserir Plataforma
+                    $nombre = $juego['Plataforma'];
+                    $nombres = explode(', ', $nombre);
+
+                    foreach ($nombres as $plataforma) {
+
+                        $resultado = $conn->query("SELECT * FROM plataforma WHERE nombre = '$plataforma'");
+                        if ($resultado->rowCount() > 0) {
+                            $row = $resultado->fetch(PDO::FETCH_ASSOC);
+                            $Plata_id  = $row['id'];
+                            echo "La platafora $plataforma esta repetido <br>";
+                        } else {
+                            $conn->exec("INSERT INTO plataforma(nombre) VALUES ('$plataforma')");
+                            $Plata_id = $conn->lastInsertId();
+                            echo "La platafora $plataforma se ha insertado <br>";
+                        }
                     }
 
                     //Inserir Videojuego
@@ -71,11 +89,17 @@
                     $nombreModificado = str_replace("'","´", $nombre);
                     $resultado = $conn->query("SELECT * FROM videojuego WHERE nombre = '$nombreModificado'");
                     if ($resultado->rowCount() > 0) {
-                        echo "El videojuego $nombreModificado esta repetido <br>";
+                        echo "Los videojuego estan repetidos <br>";
                         $last_id = $conn->lastInsertId();
                     } else {
                         $conn->exec("INSERT INTO videojuego(nombre, fecha_lanzamiento, pegi, id_desenvolupador) VALUES ('$nombreModificado', '$lanzamiento', 9, $Desen_id)");
+                        echo "Los videojuego estan insertaos <br>";
                     }
+
+
+
+
+
                 }
                 $last_id = $conn->lastInsertId();
                 $conn = null;
